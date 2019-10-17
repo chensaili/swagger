2be257f21a5652985a1213f6bd8eafe97346c125
chensaili <chensaili.dxy.cn>
2019-10-17 14:01:51 +0800

    init

---
+++ src/main/java/csl/swagger/demo/User.java
@@ -0,0 +1,42 @@
+package csl.swagger.demo;
+
+import io.swagger.models.auth.In;
+
+public class User {
+    private long id;
+    private String name;
+    private String password;
+    private Integer age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
